407830050f503618818cd74c2f79fdc097dea1d5
Strahinja Veljovic <strahinja.veljovic@get.local>
2023-12-13 14:47:15 +0100

    updates to deploy to heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import * as Tableau from '../../node_modules/@tableau/extensions-api-types' 
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTableModule} from '@angular/material/table';
